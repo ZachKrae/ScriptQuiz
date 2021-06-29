2398fd6b5bb4ee32179e373b54158f739fe21a70
unknown <zachakraemer@gmail.com>
2021-06-29 10:10:16 -0400

    added voice clips for Devanagari.js and Georgian.js

--- src/components/Georgian.js
+++ src/components/Georgian.js
@@ -1,168 +1,235 @@
+import georgian1 from '../voiceclips/georgian/georgian-1.mp3';
+import georgian2 from '../voiceclips/georgian/georgian-2.mp3';
+import georgian3 from '../voiceclips/georgian/georgian-3.mp3';
+import georgian4 from '../voiceclips/georgian/georgian-4.mp3';
+import georgian5 from '../voiceclips/georgian/georgian-5.mp3';
+import georgian6 from '../voiceclips/georgian/georgian-6.mp3';
+import georgian7 from '../voiceclips/georgian/georgian-7.mp3';
+import georgian8 from '../voiceclips/georgian/georgian-8.mp3';
+import georgian9 from '../voiceclips/georgian/georgian-9.mp3';
+import georgian10 from '../voiceclips/georgian/georgian-10.mp3';
+import georgian11 from '../voiceclips/georgian/georgian-11.mp3';
+import georgian12 from '../voiceclips/georgian/georgian-12.mp3';
+import georgian13 from '../voiceclips/georgian/georgian-13.mp3';
+import georgian14 from '../voiceclips/georgian/georgian-14.mp3';
+import georgian15 from '../voiceclips/georgian/georgian-15.mp3';
+import georgian16 from '../voiceclips/georgian/georgian-16.mp3';
+import georgian17 from '../voiceclips/georgian/georgian-17.mp3';
+import georgian18 from '../voiceclips/georgian/georgian-18.mp3';
+import georgian19 from '../voiceclips/georgian/georgian-19.mp3';
+import georgian20 from '../voiceclips/georgian/georgian-20.mp3';
+import georgian21 from '../voiceclips/georgian/georgian-21.mp3';
+import georgian22 from '../voiceclips/georgian/georgian-22.mp3';
+import georgian23 from '../voiceclips/georgian/georgian-23.mp3';
+import georgian24 from '../voiceclips/georgian/georgian-24.mp3';
+import georgian25 from '../voiceclips/georgian/georgian-25.mp3';
+import georgian26 from '../voiceclips/georgian/georgian-26.mp3';
+import georgian27 from '../voiceclips/georgian/georgian-27.mp3';
+import georgian28 from '../voiceclips/georgian/georgian-28.mp3';
+import georgian29 from '../voiceclips/georgian/georgian-29.mp3';
+import georgian30 from '../voiceclips/georgian/georgian-30.mp3';
+import georgian31 from '../voiceclips/georgian/georgian-31.mp3';
+import georgian32 from '../voiceclips/georgian/georgian-32.mp3';
+import georgian33 from '../voiceclips/georgian/georgian-33.mp3';
+
 const Georgian = [
     {
         id: 1,
         name: "ა",
-        latin: "a"
+        latin: "a",
+        sound: georgian1
     },
     {
         id: 2,
         name: "ბ",
-        latin: "b"
+        latin: "b",
+        sound: georgian2
     },
     {
         id: 3,
         name: "გ",
-        latin: "g"
+        latin: "g",
+        sound: georgian3
     },
     {
         id: 4,
         name: "დ",
-        latin: "d"
+        latin: "d",
+        sound: georgian4
     },
     {
         id: 5,
         name: "ე",
-        latin: "e"
+        latin: "e",
+        sound: georgian5
     },
     {
         id: 6,
         name: "ვ",
-        latin: "v"
+        latin: "v",
+        sound: georgian6
     },
     {
         id: 7,
         name: "ზ",
-        latin: "z"
+        latin: "z",
+        sound: georgian7
     },
     {
         id: 8,
         name: "თ",
-        latin: "t"
+        latin: "t",
+        sound: georgian8
     },
     {
         id: 9,
         name: "ი",
-        latin: "i"
+        latin: "i",
+        sound: georgian9
     },
     {
         id: 10,
         name: "კ",
-        latin: "k"
+        latin: "k",
+        sound: georgian10
     },
     {
         id: 11,
         name: "ლ",
-        latin: "l"
+        latin: "l",
+        sound: georgian11
     },
     {
         id: 12,
         name: "მ",
-        latin: "m"
+        latin: "m",
+        sound: georgian12
     },
     {
         id: 13,
         name: "ნ",
-        latin: "n"
+        latin: "n",
+        sound: georgian13
     },
     {
         id: 14,
         name: "ო",
-        latin: "o"
+        latin: "o",
+        sound: georgian14
     },
     {
         id: 15,
         name: "პ",
-        latin: "p"
+        latin: "p",
+        sound: georgian15
     },
     {
         id: 16,
         name: "ჟ",
-        latin: "zh"
+        latin: "zh",
+        sound: georgian16
     },
     {
         id: 17,
         name: "რ",
-        latin: "r"
+        latin: "r",
+        sound: georgian17
     },
     {
         id: 18,
         name: "ს",
-        latin: "s"
+        latin: "s",
+        sound: georgian18
     },
     {
         id: 19,
         name: "ტ",
-        latin: "t"
+        latin: "t",
+        sound: georgian19
     },
     {
         id: 20,
         name: "უ",
-        latin: "u"
+        latin: "u",
+        sound: georgian20
     },
     {
         id: 21,
         name: "ფ",
-        latin: "p"
+        latin: "p",
+        sound: georgian21
     },
     {
         id: 22,
         name: "ქ",
-        latin: "k"
+        latin: "k",
+        sound: georgian22
     },
     {
         id: 23,
         name: "ღ",
-        latin: "ɣ"
+        latin: "ɣ",
+        sound: georgian23
     },
     {
         id: 24,
         name: "ყ",
-        latin: "k"
+        latin: "k",
+        sound: georgian24
     },
     {
         id: 25,
         name: "შ",
-        latin: "sh"
+        latin: "sh",
+        sound: georgian25
     },
     {
         id: 26,
         name: "ჩ",
-        latin: "ch"
+        latin: "ch",
+        sound: georgian26
     },
     {
         id: 27,
         name: "ც",
-        latin: "ts"
+        latin: "ts",
+        sound: georgian27
     },
     {
         id: 28,
         name: "ძ",
-        latin: "dz"
+        latin: "dz",
+        sound: georgian28
     },
     {
         id: 29,
         name: "წ",
-        latin: "ts"
+        latin: "ts",
+        sound: georgian29
     },
     {
         id: 30,
         name: "ჭ",
-        latin: "ch"
+        latin: "ch",
+        sound: georgian30
     },
     {
         id: 31,
         name: "ხ",
-        latin: "kh"
+        latin: "kh",
+        sound: georgian31
     },
     {
         id: 32,
         name: "ჯ",
-        latin: "j"
+        latin: "j",
+        sound: georgian32
     },
     {
         id: 33,
         name: "ჰ",
-        latin: "h"
+        latin: "h",
+        sound: georgian33
     }
 ]
 
